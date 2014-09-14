2df31cb0b58d927a8cdaeea0b3ed41bcfe5817ae
wangchen <wangvsa@163.com>
2014-09-14 16:55:25 +0800

    保持被绑定的activity记录是否结束，解决dimiss的bug

--- SimpleHUD/src/info/wangchen/simplehud/SimpleHUD.java
+++ SimpleHUD/src/info/wangchen/simplehud/SimpleHUD.java
@@ -1,51 +1,52 @@
 package info.wangchen.simplehud;
 
-import android.content.Context;
+import android.app.Activity;
 import android.os.Handler;
 import android.os.Message;
-import info.wangchen.simplehud.R;
 
 public class SimpleHUD {
 	
 	private static SimpleHUDDialog dialog;
+	private static Activity activity;		
 	
 	public static final int TYPE_BLOCK = 0;
 	public static final int TYPE_NONBLOCK = 1;
 
-	public static void showLoadingMessage(Context context, String msg, int blocktype) {
+	public static void showLoadingMessage(Activity activity, String msg, int blocktype) {
 		dismiss();
-		setDialog(context, msg, R.drawable.simplehud_spinner, TYPE_BLOCK);
+		setDialog(activity, msg, R.drawable.simplehud_spinner, TYPE_BLOCK);
 		dialog.show();
 	}
 	
-	public static void showErrorMessage(Context context, String msg) {
+	public static void showErrorMessage(Activity activity, String msg) {
 		dismiss();
-		setDialog(context, msg, R.drawable.simplehud_error, TYPE_BLOCK);
+		setDialog(activity, msg, R.drawable.simplehud_error, TYPE_BLOCK);
 		dialog.show();
 		dismissAfter2s();
 	}
 
-	public static void showSuccessMessage(Context context, String msg) {
+	public static void showSuccessMessage(Activity activity, String msg) {
 		dismiss();
-		setDialog(context, msg, R.drawable.simplehud_success, TYPE_BLOCK);
+		setDialog(activity, msg, R.drawable.simplehud_success, TYPE_BLOCK);
 		dialog.show();
 		dismissAfter2s();
 	}
 	
-	public static void showInfoMessage(Context context, String msg) {
+	public static void showInfoMessage(Activity activity, String msg) {
 		dismiss();
-		setDialog(context, msg, R.drawable.simplehud_info, TYPE_BLOCK);
+		setDialog(activity, msg, R.drawable.simplehud_info, TYPE_BLOCK);
 		dialog.show();
 		dismissAfter2s();
 	}
 	
 
 	
-	private static void setDialog(Context ctx, String msg, int resId, int blocktype) {
-		//if(dialog==null)
-		dialog = SimpleHUDDialog.createDialog(ctx);
+	private static void setDialog(Activity act, String msg, int resId, int blocktype) {
+		dialog = SimpleHUDDialog.createDialog(activity);
 		dialog.setMessage(msg);
-		dialog.setImage(ctx, resId);
+		dialog.setImage(activity, resId);
+		
+		activity = act;
 
 		if(blocktype==TYPE_BLOCK)
 			dialog.setCanceledOnTouchOutside(false);
@@ -54,9 +55,12 @@ public class SimpleHUD {
 	}
 
 	public static void dismiss() {
-		if(dialog!=null && dialog.isShowing()) {
-			dialog.dismiss();
+		// 如果被绑定的activity已经结束了调用dismiss会出错
+		if(activity==null || activity.isDestroyed()) {
+			return;
 		}
+		if(dialog!=null && dialog.isShowing())
+			dialog.dismiss();
 	}
 
 
